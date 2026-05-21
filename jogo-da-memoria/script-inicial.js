9a933971b8cdcb5415f21cae4549e25e6a65d596
tomada-de-decisao-sustentavel <tomadadedecisaosustentavel@gmail.com>
2026-05-21 11:16:22 -0300

    Add files via upload

---
+++ jogo-da-memoria/script-inicial.js
@@ -0,0 +1,13 @@
+function startIntro(){
+
+    document.getElementById('intro').style.display = 'none';
+}
+function startGame(){
+
+    window.location.href = 'index.html';
+}
+function entrarJogo(){
+
+    window.location.href = 'index.html';
+
+}
