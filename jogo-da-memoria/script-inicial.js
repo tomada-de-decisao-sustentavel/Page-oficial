3c422d91dc0b6a01e4e2b8bf6a16136ea249bf64
tomada-de-decisao-sustentavel <tomadadedecisaosustentavel@gmail.com>
2026-05-21 13:47:22 -0300

    Update script-inicial.js

--- jogo-da-memoria/script-inicial.js
+++ jogo-da-memoria/script-inicial.js
@@ -3,5 +3,5 @@ function startIntro(){
     document.getElementById('intro').style.display = 'none';
 }
 function entrarJogo(){
-    window.location.href = "/Page-oficial/jogo-da-memoria/play/index.html";
+    window.location.href = "play/index.html";
 }
